f05cba0b060cf15dbae5441ff878a86c1e86fb24
learningpath2025 <learningpath2025@outlook.com>
2025-09-08 09:36:32 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Assignment3._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InputAndCountSpaces();
+        }
+
+        static void InputAndCountSpaces()
+        {
+            Console.Write("Please input a string: ");
+            string? input = Console.ReadLine();
+            if (input == null)
+            {
+                Console.WriteLine("No input provided.");
+                return;
+            }
+            int spaceCount = CountSpaces(input);
+            Console.WriteLine($"\"{input}\" contains {spaceCount} spaces");
+        }
+
+        static int CountSpaces(string input)
+        {
+            int spaceCount = 0;
+            foreach (char c in input)
+            {
+                if (c == ' ')
+                {
+                    spaceCount++;
+                }
+            }
+            return spaceCount;
+        }
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Assignment3.1.3
+
+- Write a program in C# Sharp to create a function to input a string and count number of spaces are in the string.
+- Test Data :
+	- Please input a string : This is a test string.
+- Expected Output :
+	- "This is a test string." contains 4 spaces
